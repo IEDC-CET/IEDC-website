bc740158c0030145f66d20788e70d1ac03dcd507
Anirudh A V <anirudh.av02@gmail.com>
2022-11-16 17:25:57 +0530

    Card Format changed

--- src/components/ignites/EventCard.js
+++ src/components/ignites/EventCard.js
@@ -1,24 +1,27 @@
 import React from "react";
-import "../layout/upcomcard.css";
+import "./styles.css";
 import { urlFor } from "../../client";
 
 const EventCard = ({ data }) => {
+
+  const date = () => {
+    let event = data.date
+    return new Date(event).toLocaleDateString()
+  }
+
   return (
-    <div className="upcoming-events">
-      <div className="top-ue">
+    <div className="upcom">
+      <div className="top-u">
         <img src={urlFor(data.imgUrl)} alt="" />
       </div>
       <div className="bottom-ue flex flex-col justify-center items-center">
         <span className="text-base text-justify mt-2 font-semibold" >{data.name}</span>
-        {/* <p className="mt-3 mb-0 text-sm">Date: {data.date}</p> */}
-        <p className="mt-3 mb-0 text-sm">Date: {data.date}</p>
-        {/* <p className="mt-3 mb-0 text-sm">Date: {`${data.date.getUTCDate()} / ${data.date.getUTCMonth()} / ${data.date.getUTCFullYear()}`}</p>
-        <p className="mb-0 text-sm">Time: {`${data.date.getUTCHours() < 12 ? data.date.getUTCHours() : data.date.getUTCHours() - 12}:${data.date.getUTCMinutes()} ${data.date.getUTCHours() < 12 ? "AM" : "PM"}`}</p> */}
+        <p className="mt-3 mb-0 text-sm">Date: {date()}</p>
         <p className="mb-0 text-sm">Time: {data.time}</p>
         <p className="mt-3 mb-0 text-sm">Venue: {data.location}</p>
         <a className="mt-3 mb-0 text-sm cursor-pointer bg-slate-50 p-1 rounded-md " href={data.link} target="_blank" rel="noreferrer">
-            <p className="text-black m-0 p-0">Register</p>
-            </a>
+          <p className="text-black m-0 p-0">Register</p>
+        </a>
       </div>
     </div>
   );
--- src/components/ignites/Events.js
+++ src/components/ignites/Events.js
@@ -97,15 +97,15 @@ const Events = ({
                             modules={[Autoplay, Pagination, Navigation]}
                             breakpoints={{
                                 640: {
-                                    slidesPerView: 2,
+                                    slidesPerView: 1,
                                     spaceBetween: 20,
                                 },
                                 768: {
-                                    slidesPerView: 2,
+                                    slidesPerView: 1,
                                     spaceBetween: 30,
                                 },
                                 1024: {
-                                    slidesPerView: 3,
+                                    slidesPerView: 2,
                                     spaceBetween: 20,
                                 },
                             }}
